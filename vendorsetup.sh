8bebf308e0f25a2744e4b4ae67655028af37259f
Mostafameesr <133229718+Mostafameesr@users.noreply.github.com>
2026-03-07 17:05:30 +0200

    Update vendor setup for R12.1 version

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -52,9 +52,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
         export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 
-	# R11.1 Settings
-	export FOX_VERSION="R11.1_7"
-	export OF_MAINTAINER="Aryan"
+	# R12.1 Settings
+	export FOX_MAINTAINER_PATCH_VERSION="1"
+	export OF_MAINTAINER="MOSTAFAMEESR"
 
 else
 	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
