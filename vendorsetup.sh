0bac175c75dbd7a3febc8ee037e66dee8996c2b2
Mostafameesr <133229718+Mostafameesr@users.noreply.github.com>
2026-03-08 13:26:30 +0200

    Update maintainer patch version to 1.1_Neom

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -53,7 +53,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 
 	# R12.1 Settings
-	export FOX_MAINTAINER_PATCH_VERSION="1"
+	export FOX_MAINTAINER_PATCH_VERSION="1.1_Neom"
 	export OF_MAINTAINER="MOSTAFAMEESR"
 
 		# ==========================================
