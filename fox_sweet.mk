01bd2ba6f8910f6bc2cf98a6f60f9139e2dbc2b4
Mostafameesr <133229718+Mostafameesr@users.noreply.github.com>
2026-03-13 15:42:46 +0200

    Modify backup list and DM verity settings
    Updated quick backup list to include '/dtbo' and modified DM verity setting for OTA updates.

--- fox_sweet.mk
+++ fox_sweet.mk
@@ -2,21 +2,6 @@
 #	This file is part of the OrangeFox Recovery Project
 # 	Copyright (C) 2021-2023 The OrangeFox Recovery Project
 #
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
 
 # OrangeFox-specific settings
 OF_USE_GREEN_LED := 0
@@ -26,13 +11,13 @@ OF_NO_TREBLE_COMPATIBILITY_CHECK := 1
 OF_NO_MIUI_PATCH_WARNING := 1
 
 OF_ENABLE_LPTOOLS := 1
-OF_QUICK_BACKUP_LIST := /boot;/data;
+OF_QUICK_BACKUP_LIST := /boot;/data;/dtbo;
 
 # avb 2.0
 OF_PATCH_AVB20 := 1
 
-# OTA
-OF_KEEP_DM_VERITY := 1
+# OTA (Modified for custom ROMs & Decryption)
+OF_KEEP_DM_VERITY := 0
 OF_SUPPORT_ALL_BLOCK_OTA_UPDATES := 1
 OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR := 1
 OF_DISABLE_MIUI_OTA_BY_DEFAULT := 1
