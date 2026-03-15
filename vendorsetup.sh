9633947a70406ec2b1820925ae06e85c7a9bfacf
Mostafameesr <133229718+Mostafameesr@users.noreply.github.com>
2026-03-15 19:07:57 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -41,7 +41,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_ENABLE_FRP_ADDON="1"
 
 	# R12.1 Settings
-	export FOX_MAINTAINER_PATCH_VERSION="6"
+	export FOX_MAINTAINER_PATCH_VERSION="1"
 	export OF_MAINTAINER="MOSTAFAMEESR"
 
 		# ==========================================
